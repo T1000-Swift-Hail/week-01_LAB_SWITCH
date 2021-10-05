bcde25304f588f6668cff9f8d8befcfdebd0f3a2
Njoud Alrshidi <njoudalrshidi@MacBook-Pro-Njoud.local>
2021-10-05 14:25:17 +0300

    Lap SWITCH Done

---
+++ conditionals_lab_switch.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let colorBlue = "Blue"
+let colorRed = "Red"
+let colorPicked = "Red"
+
+switch colorPicked {
+case "Red":
+    
+    print("Yes this is Picked color ")
+    
+    
+case "Blue":
+    
+    print("Not the picked color! ")
+    
+default:
+   print("color picked is not known")
+   }
+    
+    
+    
+
