981fa413c43c1666d2f742413e8d8e8a5712d93f
MACBOOK <macbook@Az-MacBook-Pro.local>
2021-10-05 14:59:17 +0300

    Finished the Exercies

---
+++ HWswitch.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+let colorBlue = "Blue"
+let colorRed = "Red"
+let colorPicked = "Red"
+
+
+
+switch colorPicked {
+    
+case colorBlue:
+    print("Yes! this is the picked color")
+    
+case colorRed :
+    print("yes this is the picked color")
+    
+default:
+    print("color picked is not known")
+    
+    
+    
+}
