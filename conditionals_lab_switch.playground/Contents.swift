a2005146586280db19199670a60da5f8f35dd306
Aryaf AlQabali <Aryaf.Aqeel.has.s@tuwaiq.edu.sa>
2021-10-05 14:25:55 +0300

    done LAB_SWITCH

---
+++ conditionals_lab_switch.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let colorBlue = "Blue"
+let colorRed = "Red"
+let colorPicked = "Red"
+
+switch colorPicked {
+case "Red" :
+    print("Yes! this is the picked color")
+case "Blue" :
+    print("Not the picked color!")
+default:
+    print("color picked is not known")
+    
+}
