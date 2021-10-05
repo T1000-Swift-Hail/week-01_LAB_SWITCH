a7113262cdd5eba096ef0db9d6d0c689a0929c45
Hind Aljehani <Hind.Aljehani.has.s@tuwaiq.edu.sa>
2021-10-05 14:49:58 +0300

    done lab

---
+++ conditionals_lab_switch.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var colorpicked = "Red"
+
+switch colorpicked {
+case "Blue":
+    print("not the picked color!")
+case "Red":
+    print("yes! this is the picked color!")
+    
+default:
+    print(" color picked is not known")
+}
+
