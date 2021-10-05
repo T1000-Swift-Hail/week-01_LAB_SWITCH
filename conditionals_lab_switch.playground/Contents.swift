4d78bc4a9be7c455247d23e2c0d22d2e3a203ee9
user <user@MacBook-Air-.local>
2021-10-05 14:25:13 +0300

    done homework

---
+++ conditionals_lab_switch.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var greeting = "Hello, playground"
+let colorPicked = "Red"
+switch colorPicked {
+case "Red":
+    print("Yes! this is the picked color")
+case "Blue":
+    print("color picked is not known")
+default:
+    print("colorPicked is not known")
+}
