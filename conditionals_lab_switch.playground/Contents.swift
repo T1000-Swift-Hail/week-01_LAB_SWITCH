34b6acde175e43bcbe6f93b6d0c5d76f91e98e8e
موضي الحربي <modhialhrbi@MacBook-Air-mwdy.local>
2021-10-05 14:37:08 +0300

    DN lab

---
+++ conditionals_lab_switch.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+let colorBlue = "Blue"
+let colorRed = "Red"
+let colorPicked = "Red"
+
+switch colorPicked  {
+case colorRed :
+    print("Yes! this is the picked color")
+case colorBlue :
+    print ("Not the picked color!")
+default :
+    print("color picked is not known")
+    }
+
+
