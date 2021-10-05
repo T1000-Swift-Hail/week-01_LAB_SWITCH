bc5714f9d165b721f5b43cea33ff50b4d2942bd7
hesah <hesah.raja.has.s@tuwaiq.edu.sa>
2021-10-05 14:29:13 +0300

    done home work switch

---
+++ conditionals_lab_switch.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Cocoa
+
+let colorBlue = "Blue"
+
+let colorRed = "Red"
+
+let colorPicked = "Red"
+
+switch colorPicked {
+case "Red" :
+    print ("Yes! this is the Picked color ")
+case "Blue" :
+ print ("No the Picked color")
+
+
+default:
+    print("color Picked is not know")
+}
+
+
+
