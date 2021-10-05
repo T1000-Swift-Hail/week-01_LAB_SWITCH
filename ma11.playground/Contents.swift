e2c54ad8b034905a6139a33b3cb8ef78f74ed825
Mastorah <mas@Mastorahs-MacBook-Air.local>
2021-10-05 14:43:39 +0300

    finish lab

---
+++ ma11.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+
+let  colorPicked : String = "Red"
+switch colorPicked{
+    
+case "Red":
+print("Yes! this is the picked color")
+case "Blue":
+print ("Not the picked color!")
+default:
+print("color picked is not known")
+}
