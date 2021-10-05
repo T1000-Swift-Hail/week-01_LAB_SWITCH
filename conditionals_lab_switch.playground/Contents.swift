b3dbd37c9fbc1238c06994b9aff56a55901e7cb1
Anas Hamad <anashamad@MacBook-Air-Anas.local>
2021-10-05 14:25:08 +0300

    switch ex

---
+++ conditionals_lab_switch.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+var color = "blue"
+
+switch color {
+case "blue":
+print("not the picked color")
+    
+case "red":
+    print("Yes! this is the picked color")
+    
+default:
+    print("color picked is not Known")
+
+
+}
